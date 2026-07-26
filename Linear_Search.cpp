ecfac983ad9583d3744e8e0d87ca3595ee32a7b8
AllanJJoseph <allanjj787@gmail.com>
2026-07-26 15:30:27 +0530

    Linear Search v2

--- Linear_Search.cpp
+++ Linear_Search.cpp
@@ -16,12 +16,13 @@ int main(){
         cin >> arr[i];
     }
 
+    int size = sizeof(arr) / sizeof(arr[0]);
     int index = -1;
 
     cout << "Enter the target: " << endl;
     cin >> target;
 
-    for(int i = 0; i < n; i++){
+    for(int i = 0; i < size; i++){
         if( arr[i] == target){
             index = i;
             break;
